62ea22a67248a990c7b8b70807fef30b6c2191e5
robsonsilv4 <robsonsilv410@gmail.com>
2019-09-01 10:00:29 -0300

    Habilitando cors

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const allowCors = require('./cors')
 
 const server = express()
 const port = 3003
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, () => {
   console.log(`Back-end rodando na porta ${port}.`)
