a8f605940838e8c55341fa965d9cc3d3ba8e1676
robsonsilv4 <robsonsilv410@gmail.com>
2019-09-05 08:37:42 -0300

    Adicionado bootstrap e nova estrutura

--- frontend/src/app.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './app.css'
-
-function App() {
-  return <h1>Todo App</h1>
-}
-
-export default App
---
+++ frontend/src/main/app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+import './app.css'
+
+export default props => (
+  <div className="container">
+    <h1>Todo App</h1>
+  </div>
+)
