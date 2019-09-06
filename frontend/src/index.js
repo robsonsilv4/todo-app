a1fa96f4637746e100bd4f4d7eb9cd5865e4aa3a
robsonsilv4 <robsonsilv410@gmail.com>
2019-09-06 16:06:50 -0300

    Configurado redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import { HashRouter as Router } from 'react-router-dom'
 
 import App from './main/app'
+import reducers from './main/reducers'
+
+const store = createStore(reducers)
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root'),
 )
