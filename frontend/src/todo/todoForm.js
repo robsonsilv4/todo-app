046dd0f5eafcc0f8b6f18dd870c4773d4c24e2b1
robsonsilv4 <robsonsilv410@gmail.com>
2019-09-06 14:41:55 -0300

    Adicionado teclas de atalho

--- frontend/src/todo/todoForm.js
+++ frontend/src/todo/todoForm.js
@@ -3,30 +3,41 @@ import React from 'react'
 import Grid from '../template/grid'
 import Button from '../template/button'
 
-export default props => (
-  <div className="todo-form">
-    <Grid cols="12 9 10">
-      <input
-        id="description"
-        className="form-control"
-        placeholder="Nome da tarefa..."
-        onChange={props.handleChange}
-        value={props.description}
-      />
-    </Grid>
+export default props => {
+  const keyHandler = e => {
+    if (e.key === 'Enter') {
+      e.shiftKey ? props:handleSearch() : props.handleAdd()
+    } else if (e.key === 'Escape') {
+      props.handleClear()
+    }
+  }
 
-    <Grid cols="12 3 2">
-      <Button
-        style="primary"
-        text="Adicionar"
-        onClick={props.handleAdd}
-      ></Button>
-      <Button
-        style="info"
-        text="Pesquisar"
-        onClick={props.handleSearch}
-      ></Button>
-      <Button text="Limpar" onClick={props.handleClear}></Button>
-    </Grid>
-  </div>
-)
+  return (
+    <div className="todo-form">
+      <Grid cols="12 9 10">
+        <input
+          id="description"
+          className="form-control"
+          placeholder="Nome da tarefa..."
+          onChange={props.handleChange}
+          onKeyUp={keyHandler}
+          value={props.description}
+        />
+      </Grid>
+
+      <Grid cols="12 3 2">
+        <Button
+          style="primary"
+          text="Adicionar"
+          onClick={props.handleAdd}
+        ></Button>
+        <Button
+          style="info"
+          text="Pesquisar"
+          onClick={props.handleSearch}
+        ></Button>
+        <Button text="Limpar" onClick={props.handleClear}></Button>
+      </Grid>
+    </div>
+  )
+}
